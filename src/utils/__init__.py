067cf110f28a92a18f31b8832ba72c086e84ae43
tanbjs <benjaset.m@gmail.com>
2026-05-21 15:33:21 +0700

    feat(utils): re-export path generators and plot helpers from package

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,16 +1,27 @@
-"""Shared utilities: AUV kinematics primitives and 6-DOF dynamics."""
+"""Shared utilities: AUV kinematics, 6-DOF dynamics, IEEE Access plotting."""
 
 from .dynamics import UUVParams, f_dyn, get_effective_buoyancy, load_params, rk4_step
 from .kinematics import eulerang, gvect, m2c, skew_symmetric
+from .path_gen import figure8_path, zigzag_path
+from .plot import (
+    COL1, COL2, GRID_FIGSIZE,
+    COLOR_DMDC, COLOR_EDMDC, COLOR_NONLINEAR, PHASE_LINE_COLOR,
+    LABELS_ETA_ANG, LABELS_ETA_LIN, LABELS_NU_ANG, LABELS_NU_LIN,
+    LABELS_TAU_F, LABELS_TAU_T,
+    add_panel_labels, apply_ieee_style, format_figure,
+)
 
 __all__ = [
-    "UUVParams",
-    "eulerang",
-    "f_dyn",
-    "get_effective_buoyancy",
-    "gvect",
-    "load_params",
-    "m2c",
-    "rk4_step",
-    "skew_symmetric",
+    # dynamics
+    "UUVParams", "f_dyn", "get_effective_buoyancy", "load_params", "rk4_step",
+    # kinematics
+    "eulerang", "gvect", "m2c", "skew_symmetric",
+    # reference trajectories
+    "figure8_path", "zigzag_path",
+    # plot
+    "COL1", "COL2", "GRID_FIGSIZE",
+    "COLOR_DMDC", "COLOR_EDMDC", "COLOR_NONLINEAR", "PHASE_LINE_COLOR",
+    "LABELS_ETA_ANG", "LABELS_ETA_LIN", "LABELS_NU_ANG", "LABELS_NU_LIN",
+    "LABELS_TAU_F", "LABELS_TAU_T",
+    "add_panel_labels", "apply_ieee_style", "format_figure",
 ]
